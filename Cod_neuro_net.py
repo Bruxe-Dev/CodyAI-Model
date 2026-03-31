84a99966cc98e3a4debc285da3bf15fa8d818fd6
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-31 14:29:40 +0200

    removed unwanted comments

--- Cod_neuro_net.py
+++ Cod_neuro_net.py
@@ -56,8 +56,6 @@ class NeuralNetwork:
 if __name__ == "__main__":
     net = NeuralNetwork(input_size=11, hidden_size=256, output_size=3)
 
-    # Let's teach it one simple rule:
-    # Given this state → action 2 (turn right) should score highest
     fake_state  = np.array([1,0,0,1,0,0,1,0,0,0,1], dtype=float)
     fake_target = np.array([0.0, 1.0, 0.0])  # we WANT output 2 to be 1.0
 
