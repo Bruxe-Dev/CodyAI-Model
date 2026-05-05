a51521fc35a84e352c1e083b25fbbaff1002c6f0
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-05 11:51:27 +0200

    initialized the trading environent for the model

--- src/trade_env.py
+++ src/trade_env.py
@@ -5,8 +5,11 @@ from ta.momentum import RSIIndicator
 from ta.trend import MACD
 from enum import Enum
 
-class tradeAction(Enum):
+class tradingAction(Enum):
     HOLD = 0
     BUY = 1
     SELL = 2
 
+class tradingEnv:
+    def __init__(self,stock_data, initial_balance = 10000,commission= 0.001):
+        
