451d94bcf7eb969015f314c67fbfaa14bea747de
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-05 13:16:00 +0200

    added the Supaparameters

--- src/trading_agent.py
+++ src/trading_agent.py
@@ -14,4 +14,13 @@ class TradingDQNAgent:
             self.hidden_size,
             self.output_size
         )
-        
+
+        self.memory = deque (maxlen=100_000)
+
+        self.gamma = 0.95 # Discount (Model focuses on future rewards) - Adjust this, higher = more focus on future rewards
+        self.epsilon = 1.0 # Exploration rate (from 1.0 to 100% each try) - Adjust this, higher = more exploration
+        self.epsilon_min = 0.01 # The agent must at least explore 1% for each try
+        self.epsilon_decay = 0.995 # Ensures Exploration and Exploitation
+        self.learning_rate = 0.0001 # How the NN updates its bias and Weight - better understanding that, it's the steps the gradient takes per update 😂
+        self.batch_size = 64
+
